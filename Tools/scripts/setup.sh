4df4931e52bc25754e7e1005a0960f4226d1127b
TanakaLun <2709410138@qq.com>
2026-05-22 17:03:36 +0800

    make yuki great again

---
+++ Tools/scripts/setup.sh
@@ -0,0 +1,37 @@
+          sed -i 's/NagramX/YukinoGram/g' settings.gradle
+          sed -i 's/NagramX/YukinoGram/g' TMessagesProj/build.gradle
+          sed -i 's/Nagram X/YukinoGram/g' TMessagesProj/src/main/java/tw/nekomimi/nekogram/utils/AndroidUtil.java
+          sed -i 's/nu\.gpu\.nagram/io.tl.yukinogram/g' gradle.properties
+          MANIFEST_PATH="TMessagesProj/src/main/AndroidManifest.xml"
+          sed -i 's/android:icon="@mipmap\/ic_launcher_nagram_blue"/android:icon="@mipmap\/ic_launcher_yukino"/g' "$MANIFEST_PATH"
+          sed -i 's/android:label="@string\/NagramX"/android:label="@string\/YukinoGram"/g' "$MANIFEST_PATH"
+          sed -i '/<!-- usesCleartextTraffic for browser supporting opening http:\/\/ links -->/a\
+        <activity-alias\
+            android:enabled="true"\
+            android:name="org.telegram.messenger.YukinoIcon"\
+            android:targetActivity="org.telegram.ui.LaunchActivity"\
+            android:icon="@mipmap/ic_launcher_yukino"\
+            android:exported="true">\
+\
+            <intent-filter>\
+                <action android:name="android.intent.action.MAIN" />\
+                <category android:name="android.intent.category.LAUNCHER" />\
+                <category android:name="android.intent.category.MULTIWINDOW_LAUNCHER" />\
+            </intent-filter>\
+            <meta-data android:name="android.app.shortcuts" android:resource="@xml/shortcuts" />\
+        </activity-alias>\
+\
+        <activity-alias\
+            android:enabled="true"\
+            android:name="org.telegram.messenger.MonetYukinoIcon"\
+            android:targetActivity="org.telegram.ui.LaunchActivity"\
+            android:icon="@mipmap/ic_launcher_yukino_monet"\
+            android:exported="true">\
+\
+            <intent-filter>\
+                <action android:name="android.intent.action.MAIN" />\
+                <category android:name="android.intent.category.LAUNCHER" />\
+                <category android:name="android.intent.category.MULTIWINDOW_LAUNCHER" />\
+            </intent-filter>\
+            <meta-data android:name="android.app.shortcuts" android:resource="@xml/shortcuts" />\
+        </activity-alias>' "$MANIFEST_PATH"
